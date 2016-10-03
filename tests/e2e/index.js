bbb208178936607b0d8fbb5c2083b9451f3db566
wblankenship <william.jblankenship@gmail.com>
2016-10-03 14:30:32 -0400

    Integration tests across autoaccept email

--- tests/e2e/index.js
+++ tests/e2e/index.js
@@ -1,17 +1,18 @@
 'use strict';
 
-var test = require('tape');
-var request = require('request');
-var SMTPServer = require('smtp-server').SMTPServer;
-var index = require('../../index.js');
-var config = require('../../config');
-var uuid = require('node-uuid').v4();
-var db = require('../../db.js');
+var test = require(`tape`);
+var request = require(`request`);
+var index = require(`../../index.js`);
+var config = require(`../../config`);
+var uuid = require(`node-uuid`).v4();
+var db = require(`../../db.js`);
+var http = require(`http`);
 
 // Use the same server for all tests
-var server = index('8080');
+var port = 8000
+var server = index(port);
 
-test('Creating user sends email', function (t) {
+test(`Creating user sends email`, function (t) {
   var reqOpts = {
     auth: {
       'user': config.heroku.id,
@@ -19,62 +20,53 @@ test('Creating user sends email', function (t) {
     },
     body: {
       'uuid': uuid,
-      'plan': 'free'
+      'plan': `free`
     },
     json: true
   };
 
-  var smtpServer = new SMTPServer({
-    onData: function dataHandler (stream, session, cb) {
-      cb();
-      t.pass('Email received');
-      smtpServer.close();
-      return t.end();
-    },
-    onAuth: function (a, s, cb) { return cb(null, { user: 'fake' }); },
-    tls: {
-      rejectUnauthorized: false
-    },
-    authOptional: true,
-    closeTimeout: 0,
-  });
+  // Start a server to wait for the authentication request
+  var callback = http.createServer(function (req) {
+    t.pass('Received callback!')
+    callback.close(function () {
+      t.end()
+    })
+  }).listen(8080)
 
-  smtpServer.listen(25, '0.0.0.0');
-
-  request.post('http://127.0.0.1:8080/heroku/resources',
+  request.post(`http://127.0.0.1:${port}/heroku/resources`,
     reqOpts,
     function userCreated (e, res) {
       t.error(e);
-      t.equal(res.statusCode, 200, 'Created user');
+      t.equal(res.statusCode, 200, `Created user`);
   });
 });
 
-test('Updating user changes user plan in db', function (t) {
+test(`Updating user changes user plan in db`, function (t) {
   var reqOpts = {
     auth: {
       'user': config.heroku.id,
       'pass': config.heroku.password
     },
     body: {
-      'plan': 'foobar',
+      'plan': `foobar`,
     },
     json: true
   };
 
-  var url = `http://127.0.0.1:8080/heroku/resources/${uuid}`;
+  var url = `http://127.0.0.1:${port}/heroku/resources/${uuid}`;
   request.put(url, reqOpts, function (e, res) {
     t.error(e);
-    t.equal(res.statusCode, 200, 'Update worked');
+    t.equal(res.statusCode, 200, `Update worked`);
     db.get({ id: uuid }, function (e, doc) {
       t.error(e);
       if(e) { throw e; }
-      t.equal(doc.tier, 'foobar');
+      t.equal(doc.tier, `foobar`);
       t.end();
     });
   });
 });
 
-test('Deleting user removes user from the database', function (t) {
+test(`Deleting user removes user from the database`, function (t) {
   var reqOpts = {
     auth: {
       'user': config.heroku.id,
@@ -83,15 +75,15 @@ test('Deleting user removes user from the database', function (t) {
     json: true
   };
 
-  var url = `http://127.0.0.1:8080/heroku/resources/${uuid}`;
+  var url = `http://127.0.0.1:${port}/heroku/resources/${uuid}`;
   request.delete(url, reqOpts, function (e, res) {
     t.error(e);
-    t.equal(res.statusCode, 200, 'User removed');
+    t.equal(res.statusCode, 200, `User removed`);
     t.end();
   });
 });
 
-test('Deleting user twice fails', function (t) {
+test(`Deleting user twice fails`, function (t) {
   var reqOpts = {
     auth: {
       'user': config.heroku.id,
@@ -100,46 +92,46 @@ test('Deleting user twice fails', function (t) {
     json: true
   };
 
-  var url = `http://127.0.0.1:8080/heroku/resources/${uuid}`;
+  var url = `http://127.0.0.1:${port}/heroku/resources/${uuid}`;
   request.delete(url, reqOpts, function (e, res) {
     t.error(e);
-    t.equal(res.statusCode, 503, 'User can\'t be removed');
+    t.equal(res.statusCode, 503, `User can\`t be removed`);
     t.end();
   });
 });
 
-test('Auth is required', function (t) {
+test(`Auth is required`, function (t) {
   var reqOpts = {
     json: true
   };
 
-  var url = `http://127.0.0.1:8080/heroku/resources/${uuid}`;
+  var url = `http://127.0.0.1:${port}/heroku/resources/${uuid}`;
   request.delete(url, reqOpts, function (e, res) {
     t.error(e);
-    t.equal(res.statusCode, 401, 'Auth fails');
+    t.equal(res.statusCode, 401, `Auth fails`);
     t.end();
   });
 });
 
-test('Auth must match', function (t) {
+test(`Auth must match`, function (t) {
   var reqOpts = {
     auth: {
-      'user': 'wrong',
-      'pass': 'wrong'
+      'user': `wrong`,
+      'pass': `wrong`
     },
     json: true
   };
 
-  var url = `http://127.0.0.1:8080/heroku/resources/${uuid}`;
+  var url = `http://127.0.0.1:${port}/heroku/resources/${uuid}`;
   request.delete(url, reqOpts, function (e, res) {
     t.error(e);
-    t.equal(res.statusCode, 401, 'Auth fails');
+    t.equal(res.statusCode, 401, `Auth fails`);
     t.end();
   });
 });
 
-test('Teardown', function (t) {
-  t.pass('Cleaning up server');
+test(`Teardown`, function (t) {
+  t.pass(`Cleaning up server`);
   server.close();
   t.end();
 });
